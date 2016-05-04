0ccb88f0e444e1a7be451c6b8addfb4de1edc2f1
Farhan Wazir <farhan.wazir@gmail.com>
2016-05-05 02:38:12 +0500

    fixing

--- src/FarhanWazir/GoogleMaps/example/Controllers/MapController.php
+++ src/FarhanWazir/GoogleMaps/example/Controllers/MapController.php
@@ -17,41 +17,7 @@ class MapController extends Controller
     }
 
     public function index(){
-
-        /******** Custom Map Controls ********/
-
-        $leftTopControls = ['document.getElementById("leftTopControl")']; // values must be html or javascript element
-        $this->gmap->injectControlsInLeftTop = $leftTopControls; // inject into map
-        $leftCenterControls = ['document.getElementById("leftCenterControl")'];
-        $this->gmap->injectControlsInLeftCenter = $leftCenterControls;
-        $leftBottomControls = ['document.getElementById("leftBottomControl")'];
-        $this->gmap->injectControlsInLeftBottom = $leftBottomControls;
-
-        $bottomLeftControls = ['document.getElementById("bottomLeftControl")'];
-        $this->gmap->injectControlsInBottomLeft = $bottomLeftControls;
-        $bottomCenterControls = ['document.getElementById("bottomCenterControl")'];
-        $this->gmap->injectControlsInBottomCenter = $bottomCenterControls;
-        $bottomRightControls = ['document.getElementById("bottomRightControl")'];
-        $this->gmap->injectControlsInBottomRight = $bottomRightControls;
-
-        $rightTopControls = ['document.getElementById("rightTopControl")'];
-        $this->gmap->injectControlsInRightTop = $rightTopControls;
-        $rightCenterControls = ['document.getElementById("rightCenterControl")'];
-        $this->gmap->injectControlsInRightCenter = $rightCenterControls;
-        $rightBottomControls = ['document.getElementById("rightBottomControl")'];
-        $this->gmap->injectControlsInRightBottom = $rightBottomControls;
-
-        $topLeftControls = ['document.getElementById("topLeftControl")'];
-        $this->gmap->injectControlsInTopLeft = $topLeftControls;
-        $topCenterControls = ['document.getElementById("topCenterControl")'];
-        $this->gmap->injectControlsInTopCenter = $topCenterControls;
-        $topRightControls = ['document.getElementById("topRightControl")'];
-        $this->gmap->injectControlsInTopRight = $topRightControls;
-
-        /******** End Controls ********/
-
         $config = array();
-        $config['map_height'] = "100%";
         $config['center'] = 'Defence Garden, Karachi';
         $config['onboundschanged'] = 'if (!centreGot) {
             var mapCentre = map.getCenter();
@@ -61,14 +27,14 @@ class MapController extends Controller
         }
         centreGot = true;';
 
-        $this->gmap->initialize($config); // Initialize Map with custom configuration
+        $this->gmap->initialize($config);
 
         // set up the marker ready for positioning
+        // once we know the users location
         $marker = array();
-        $marker['draggable'] = true;
         $this->gmap->add_marker($marker);
 
-        $map = $this->gmap->create_map(); // This object will render javascript files and map view; you can call JS by $map['js'] and map view by $map['html']
+        $map = $this->gmap->create_map();
 
         return view('map', ['map' => $map]);
     }
--- src/FarhanWazir/GoogleMaps/example/Routes/routes.php
+++ src/FarhanWazir/GoogleMaps/example/Routes/routes.php
@@ -2,12 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Example only
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
+| Do not replace with app routes.php file
 |
 */
 Route::get('/map', 'MapController@index');
